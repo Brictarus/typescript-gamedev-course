c3e346958263e01d88417eec0ec5d474ef5d727a
Brictarus <brice.lepeigneul@gmail.com>
2026-08-31 20:54:13 +0200

    refactor: menu rendering using constants

--- src/systems/RenderSystem.ts
+++ src/systems/RenderSystem.ts
@@ -58,6 +58,6 @@ export class RenderSystem {
 
   private renderMenuBackground() {
     this.ctx.fillStyle = '#0f3460';
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
   }
 }
